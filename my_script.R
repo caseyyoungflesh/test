c70b2915f5ef6104e6b1c6e7c748dffda029046a
Casey Youngflesh <caseyyoungflesh@gmail.com>
2018-11-08 13:33:13 -0500

    Change to 1k samples

--- my_script.R
+++ my_script.R
@@ -1,19 +1,19 @@
 #My script
-a <- rnorm(100)
-b <- rnorm(100)
+a <- rnorm(1000)
+b <- rnorm(1000)
 
 plot(a, b)
 
 
 #More data sim
-c <- runif(100)
-d <- runif(100)
+c <- runif(1000)
+d <- runif(1000)
 
 plot(c, d)
 
 
 #add third set of data
-e <- rgamma(100, shape = 1)
-f <- rgamma(100, shape = 1)
+e <- rgamma(1000, shape = 1)
+f <- rgamma(1000, shape = 1)
 
 plot(e, f)
